c81ef5f309d41b99a58430aef33826303bb0f926
Gareth Jones <gareth.jones@shinetech.com>
2019-07-19 16:03:57 +1000

    chore: extracted the file name parsing and formatting

---
+++ test/fileNameParser-test.js
@@ -0,0 +1,83 @@
+const should = require("should");
+
+describe("fileNameParser", () => {
+  describe("with default options", () => {
+    const parser = require("../lib/fileNameParser")({
+      file: {
+        dir: "/path/to/file",
+        base: "thefile.log",
+        ext: ".log",
+        name: "thefile"
+      }
+    });
+    it("should return null for filenames that do not match", () => {
+      should(parser("cheese.txt")).not.be.ok();
+      should(parser("thefile.log.biscuits")).not.be.ok();
+    });
+    it("should take a filename and return the index", () => {
+      parser("thefile.log.2").should.eql({ index: 2, isCompressed: false });
+      parser("thefile.log.2.gz").should.eql({ index: 2, isCompressed: true });
+    });
+  });
+
+  describe("with pattern option", () => {
+    const parser = require("../lib/fileNameParser")({
+      file: {
+        dir: "/path/to/file",
+        base: "thefile.log",
+        ext: ".log",
+        name: "thefile"
+      },
+      pattern: "yyyy-MM-dd"
+    });
+    it("should return null for files that do not match", () => {
+      should(parser("thefile.log.biscuits")).not.be.ok();
+      should(parser("thefile.log.2019")).not.be.ok();
+      should(parser("thefile.log.3.2")).not.be.ok();
+    });
+    it("should take a filename and return the date", () => {
+      parser("thefile.log.2019-07-17").should.eql({
+        index: 0,
+        date: "2019-07-17",
+        timestamp: 1563321600000,
+        isCompressed: false
+      });
+      parser("thefile.log.gz").should.eql({
+        index: 0,
+        isCompressed: true
+      });
+    });
+    it("should take a filename and return both date and index", () => {
+      parser("thefile.log.2019-07-17.2").should.eql({
+        index: 2,
+        date: "2019-07-17",
+        timestamp: 1563321600000,
+        isCompressed: false
+      });
+      parser("thefile.log.2019-07-17.2.gz").should.eql({
+        index: 2,
+        date: "2019-07-17",
+        timestamp: 1563321600000,
+        isCompressed: true
+      });
+    });
+  });
+
+  describe("with keepFileExt option", () => {
+    const parser = require("../lib/fileNameParser")({
+      file: {
+        dir: "/path/to/file",
+        base: "thefile.log",
+        ext: ".log",
+        name: "thefile"
+      },
+      keepFileExt: true
+    });
+    it("should take a filename and return the index", () => {
+      should(parser("thefile.log.2")).not.be.ok();
+      should(parser("thefile.log.2.gz")).not.be.ok();
+      parser("thefile.2.log").should.eql({ index: 2, isCompressed: false });
+      parser("thefile.2.log.gz").should.eql({ index: 2, isCompressed: true });
+    });
+  });
+});
